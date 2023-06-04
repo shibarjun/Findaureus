a334118441ceaf4be5a9447fc0211fa07aed1d82
Shibarjun <47144889+shibarjun@users.noreply.github.com>
2023-06-04 14:33:18 +0100

    Update main.py

--- FindAureus/main.py
+++ FindAureus/main.py
@@ -1,3 +1,3 @@
-from FindAureus.FindaureusFunctions import *
+from FindaureusFunctions import *
 
 zplanes_with_bacteria, bacteria_coordinate, zplanes_with_no_bacteria, pixlewise_bacteria_coordinate, bacteria_area,metadata = FindAureus()
